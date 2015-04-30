da6b0c0373bcc80da2f51ceea9995bece05e96a2
gabrielfalcone <gabrielfalcone@gmail.com>
2015-04-30 17:04:41 -0300

    Add player model

--- push_to_master.cmd
+++ push_to_master.cmd
@@ -1,3 +1,21 @@
+@if [%1]==[] goto print_help
+
+
+git status
+@choice /M "Continue?"
+@if errorlevel 2 goto end
+
+
+:perform_push
 git add --all --verbose
 git commit -m %1
 git push origin master
+@goto end
+
+
+:print_help
+@echo Message is missing!!
+@goto end
+
+
+:end
