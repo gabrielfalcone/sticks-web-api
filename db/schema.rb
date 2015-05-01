b0be09c607fb6a40088bd3158c77f9ea90d95460
gabrielfalcone <gabrielfalcone@gmail.com>
2015-05-01 19:45:28 -0300

    Testing adding new field to a model 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501222020) do
+ActiveRecord::Schema.define(version: 20150501224337) do
 
   create_table "players", force: true do |t|
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
